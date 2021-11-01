3dd835b7e03fd6a4b71abcf89681e5d9967da190
Kshitij Kumar <physiomatica@gmail.com>
2021-11-01 22:16:05 +0530

    small changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,14 +5,29 @@ function Contact() {
     <div className="odd-page page" id="contact">
       <div className="main-heading">How to reach Me</div>
       <div className="desc-text">
-        Email: <a href="mailto:kshitijkumar176@gmail.com">kshitijkumar176@gmail.com</a>
+        Email:{" "}
+        <a
+          rel="noreferrer"
+          href="mailto:kshitijkumar176@gmail.com"
+          target="_blank"
+        >
+          kshitijkumar176@gmail.com
+        </a>
       </div>
-      <div className="desc-text">Phone: +91 9650181751 </div>
+      <div className="desc-text">
+        Phone: <a href="tel:965-018-1751">+91 9650181751</a>{" "}
+      </div>
       <div className="desc-text">
         LinkedIn:{" "}
-        <a href="https://www.linkedin.com/in/kshitij-kumar-17601/"> Kshitij</a>.
+        <a
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/kshitij-kumar-17601/"
+          target="_blank"
+        >
+          Kshitij
+        </a>
+        .
       </div>
-      
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/">
                 Home
               </a>
             </li>
